c8df1082d0f9f6e4a1e6c56eb8aaed151154e4f3
alefmeira <alef.chaves01@gmail.com>
2023-02-06 21:02:05 -0300

    [digital-bank/setup] Setup inicial digital bank

---
+++ src/main/java/com/digitalbank/accounts/infrastructure/configuration/SomeConfig.java
@@ -0,0 +1,4 @@
+package com.digitalbank.accounts.infrastructure.configuration;
+
+public class SomeConfig {
+}
---
+++ src/main/java/com/digitalbank/accounts/interfacesadapters/gateways/clients/someclient/SomeFeingClient.java
@@ -0,0 +1,4 @@
+package com.digitalbank.accounts.interfacesadapters.gateways.clients.someclient;
+
+public interface SomeFeingClient {
+}
